722f71e5696857dedaff1c0e980efb93525ecdf9
AlekseevaVA <alekseevavria@mail.ru>
2025-09-15 19:19:19 +0500

    Task3

---
+++ Tyuiu.AlekseevaVA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.AlekseevaVA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
+
